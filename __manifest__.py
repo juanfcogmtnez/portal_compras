3bd3225b8f8bdae9cda2048226889e8ae928efb2
juanfcogmtnez <juanfcogmtnez@gmail.com>
2021-10-18 15:58:33 +0000

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+{
+	'name':'Modulo portal del proveedor',
+	'version':'1.0',
+	'depends':['purchase','portal'],
+	'author':'Juan Fco Garcia',
+	'category':'Consultoría',
+	'description':'''
+	Modulo para que los proveedores envien ofertas
+	''',}
